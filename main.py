09abcf074127ed5d9db362c872f487f9dbd17ecd
akshxdevs <akshxdevs@gmail.com>
2026-05-13 17:28:22 +0530

    Fix parser typo and unpack args port_range correctly in main entry point

--- main.py
+++ main.py
@@ -4,16 +4,16 @@ from scanner import scan
 
 
 def get_args():
-    praser = argparse.ArgumentParser(description="The Port Scanner")
-    praser.add_argument("--target", required=True)
-    praser.add_argument(
+    parser = argparse.ArgumentParser(description="The Port Scanner")
+    parser.add_argument("--target", required=True)
+    parser.add_argument(
         "--start-port",
         "-sp",
         type=int,
         default=1,
         help="starting range of port (default = 1)",
     )
-    praser.add_argument(
+    parser.add_argument(
         "--end-port",
         "-np",
         type=int,
@@ -21,13 +21,13 @@ def get_args():
         help="ending range of port (default = 1)",
     )
 
-    args = praser.parse_args()
+    args = parser.parse_args()
 
     if args.start_port < 0 or args.end_port > 65535:
-        praser.error("Port must me between 0 and 65535")
+        parser.error("Port must me between 0 and 65535")
 
     if args.start_port > args.end_port:
-        praser.error(
+        parser.error(
             f"start port {args.start_port} cannot be greater than end port {args.end_port}"
         )
 
@@ -36,11 +36,10 @@ def get_args():
     print(f"Scanning Ports: {args.start_port} to {args.end_port}")
     print("Total ports: ", len(port_range))
 
-    scan(args, port_range)
-
     return args, port_range
 
 
 if __name__ == "__main__":
-    args = get_args()
+    args, port_range = get_args()
+    scan(args, port_range)
     print(args)
